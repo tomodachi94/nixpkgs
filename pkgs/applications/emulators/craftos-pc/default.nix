c7f8c68c311af5be967bc691db70f99fdbc2e5ff
Tomo <tomodachi94@protonmail.com>
2024-10-13 22:14:59 +0000

    craftos-pc: install craftos2-lua at libcraftos2-liblua.so
    This path is used by upstream packages:
    https://github.com/MCJack123/craftos2-release-resources/blob/524cdf0c47f3f59174f3f180a682f4701ed6a068/ubuntu/debian_craftos-pc/rules#L12

--- pkgs/applications/emulators/craftos-pc/default.nix
+++ pkgs/applications/emulators/craftos-pc/default.nix
@@ -67,8 +67,8 @@ stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -p $out/bin $out/lib $out/share/craftos $dev/include
     DESTDIR=$out/bin make install
-    cp ./craftos2-lua/src/liblua.so $out/lib
-    patchelf --replace-needed craftos2-lua/src/liblua.so liblua.so $out/bin/craftos
+    cp ./craftos2-lua/src/liblua.so $out/lib/libcraftos2-lua.so
+    patchelf --replace-needed craftos2-lua/src/liblua.so libcraftos2-lua.so $out/bin/craftos
     cp -R api $dev/include/CraftOS-PC
     cp craftos2-lua/include/* $dev/include/CraftOS-PC
     cp -R ${craftos2-rom}/* $out/share/craftos
